fb89c92c3230d3e99688a0467739320f71cd16b2
Your Name <your@email.com>
2021-12-06 16:31:06 -0500

    fin ISS3, changeed request API link to take string literals for IP add

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // // index.js
-// const { fetchMyIP } = require('./iss');
+const { fetchMyIP } = require('./iss');
 const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
 // fetchMyIP((error, ip) => {
 //   if (error) {
@@ -24,3 +25,16 @@ const { fetchCoordsByIP } = require('./iss');
 //   console.log('It worked! Returned coordinates:', data);
 //   //console.log(typeof data);//check type of data
 // });
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+fetchISSFlyOverTimes({ latitude: '45.4676', longitude: '-73.6529' }, (error, data) => {
+  if (error) {
+    console.log("It didn't work!", error);
+    return;
+  }
+
+  console.log('It worked! Returned flyover times:', data);
+
+});
